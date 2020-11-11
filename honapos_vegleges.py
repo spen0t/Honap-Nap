9163e60f11413dccf644bbcce64132f5a91c8deb
spen0t <71476085+spen0t@users.noreply.github.com>
2020-11-11 02:33:21 +0100

    Add files via upload
    Továbbgondoltam az eredeti feladatot kicsit. Itt nem az évek az "extra" hanem, hogy a program akkor sem ad beépített hibaüzenetet ha a nap helyére nem int kerül. Hanem az álltalam megfogalmazott üzenet jelenik meg.
    (Plusz addig loopol a program amíg hibás adatot visznek be.)

---
+++ honapos_vegleges.py
@@ -0,0 +1,39 @@
+progi=True
+
+while progi:
+    honapok= ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"]
+    napok=[31,29,31,30,31,30,31,31,30,31,30,31]
+    h=input("Hónap: ")
+    n=input("Nap: ")
+
+    def szamHiba():
+        try:
+            int(n)
+            return True
+        except ValueError:
+            return False
+
+    def napHiba():
+        nap=int(n)
+        index=honapok.index(h)
+        napmax=napok[index]
+        if nap>=napmax and nap<0:
+            return False
+
+    if szamHiba()==False:
+        print("Hiba")
+    elif h not in honapok:
+        print("Hiba")
+    elif napHiba()==False:
+        print("Hiba")
+    else:
+        print("{} {}.".format(h,n))
+
+
+
+    if szamHiba()==False or napHiba()==False:
+        progi=True
+    elif h not in honapok:
+        progi=True
+    else:
+        progi=False
